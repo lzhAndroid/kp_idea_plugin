24d10776480687b7dbf719c0f59de43658895397
GitHub Action <action@github.com>
2023-05-05 03:07:21 +0000

    Template cleanup

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,1 @@
-rootProject.name = "IntelliJ Platform Plugin Template"
+rootProject.name = "kp_idea_plugin"
--- src/main/kotlin/com/github/lzhandroid/kpideaplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/lzhandroid/kpideaplugin/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.lzhandroid.kpideaplugin.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.lzhandroid.kpideaplugin.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
